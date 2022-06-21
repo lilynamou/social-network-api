78e369ea827459513dae23ece15c727fc5e166a3
lilynamou <lilyananamou@gmail.com>
2022-06-20 23:18:01 -0700

    libraryjs

--- models/Library.js
+++ models/Library.js
@@ -1,15 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-// TODO: Define a new schema named `bookSchema` for the subdocument
+// Child documents 
+// The bookSchema defines the schema of the subdocument
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  price: Number,
+});
 
+// The librarySchema defines the schema of the parent document
 const librarySchema = new mongoose.Schema({
   name: { type: String, required: true },
-  //
-  // TODO: Add the `books` subdocument to the parent document as an array
-  //
+  // This will include an array that holds all the books
+  books: [bookSchema],
   lastAccessed: { type: Date, default: Date.now },
 });
 
-// TODO: Create a model named `Library`
+// Uses mongoose.model() to create model
+const Library = mongoose.model('Library', librarySchema);
 
 module.exports = Library;
